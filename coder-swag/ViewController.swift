338e1e531089a3897562fbadaed74a7fd1c09e5a
Philip <killfilipok@gmail.com>
2019-03-03 17:14:45 +0200

    first commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Philip on 3/3/19.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
